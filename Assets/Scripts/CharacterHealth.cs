92f12f393ddb302055e206e5f420a0af80b317f6
Safwa <safwa.tawab@gmail.com>
2019-01-27 03:00:41 +0200

    Sound Manager & Health bar

---
+++ Assets/Scripts/CharacterHealth.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterHealth : MonoBehaviour
+{
+
+    public float CurrentHealth { get; set; }
+    public float MaxHealth { get; set; }
+    public float damageAmount = 6f;
+
+    public Slider healthBar;
+
+    [SerializeField]
+    Player Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MaxHealth = 20f;
+        CurrentHealth = MaxHealth;
+
+        healthBar.value = CalculateHealth();
+
+        Player = GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.X))
+    //    {
+    //        DealDamage(damageAmount);
+    //    }
+    //}
+
+    void DealDamage(float damageValue)
+    {
+        CurrentHealth -= damageValue;
+        healthBar.value = CalculateHealth();
+
+        if (CurrentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    float CalculateHealth()
+    {
+        return CurrentHealth / MaxHealth;
+    }
+
+    void Die()
+    {
+        CurrentHealth = 0;
+        Debug.Log("Player " + Player.playerNum + " is dead.");
+    }
+}
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+    public AudioClip attackHitSound;
+    public AudioClip gettingHitSound;
+    public AudioClip jumpSound;
+    public AudioClip victorySound;
+
+    public static SoundManager inistance = null;
+
+    AudioSource audioSource = null;
+
+
+
+    void Awake()
+    {
+        inistance = this;
+        audioSource = this.GetComponent<AudioSource>();
+    }
+
+
+    public void PlayattackHitSound()
+    {
+        audioSource.PlayOneShot(attackHitSound);
+    }
+
+    public void PlaygettingHitSound()
+    {
+        audioSource.PlayOneShot(gettingHitSound);
+    }
+
+    public void PlayjumpSound()
+    {
+        audioSource.PlayOneShot(jumpSound);
+    }
+
+    public void PlayvictorySound()
+    {
+        audioSource.PlayOneShot(victorySound);
+    }
+}
